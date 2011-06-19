eaca8f3aa7dc31594fac80b1805ee79bb671b65b
Noam Etzion-Rosenberg <noam@pleasantlight.com>
2011-06-19 22:18:59 +0300

    Initial Commit

---
+++ json_kidTests/json_kidTests.h
@@ -0,0 +1,13 @@
+//
+//  json_kidTests.h
+//  json_kidTests
+//
+//  Created by נעם עציון-רוזנברג on 19/06/11.
+//  Copyright 2011 PleasantLight. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface json_kidTests : SenTestCase
+
+@end
---
+++ json_kidTests/json_kidTests.m
@@ -0,0 +1,32 @@
+//
+//  json_kidTests.m
+//  json_kidTests
+//
+//  Created by נעם עציון-רוזנברג on 19/06/11.
+//  Copyright 2011 PleasantLight. All rights reserved.
+//
+
+#import "json_kidTests.h"
+
+@implementation json_kidTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in json_kidTests");
+}
+
+@end
